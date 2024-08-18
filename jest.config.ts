daf0205855276cbe21ab3aceb891d8fffec81c81
scobru <scobru1988@gmail.com>
2024-08-19 00:38:01 +0200

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'scobru',
+  preset: './jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: './coverage/scobru',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
